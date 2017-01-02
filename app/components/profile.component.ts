4f453cf2195c96b0aa1202d2d9ec4cb51692432d
Waseem Ahmed <alphawaseem@gmail.com>
2017-01-02 18:16:59 +0530

    update : profile component

--- app/components/profile.component.ts
+++ app/components/profile.component.ts
@@ -6,7 +6,12 @@ import { GithubServiceInjectable } from './../services/github.service';
     templateUrl : 'profile.component.html'
 })
 export class ProfileComponent{
+    user : Object;
     constructor(private github:GithubServiceInjectable){
-        github.getUser().subscribe(user => console.log(user));
+        github.getUser().subscribe(user => {
+            this.user = user;
+            console.log(this.user);
+            
+        });
     }
 }
